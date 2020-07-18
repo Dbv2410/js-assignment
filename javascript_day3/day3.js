e7b24a5db72a4537132afb018c7383ecbfd09e23
Dbv2410 <63506793+Dbv2410@users.noreply.github.com>
2020-07-18 14:40:53 +0530

    Day 3 assignment

---
+++ javascript_day3/day3.js
@@ -0,0 +1,17 @@
+let num=prompt("enter the number:");
+if(num >=75)
+{
+    console.log("you have scored A grade");
+}
+else if(num<75&&num>=65){
+    console.log("you have scored grade B");
+}
+else if(num<65&&num>=55){
+    console.log("you have scored grade c");
+}
+else
+{
+    alert("ohhh! you have failed the test");
+}
+
+
---
+++ javascript_day3/day3_i.js
@@ -0,0 +1,10 @@
+let num = prompt("enter the number");
+function evenOdd(){
+    if(num%2==0){
+        console.log("the number is even");
+    }
+    else{
+        console.log("the number is odd");
+    }
+}
+evenOdd(num);
